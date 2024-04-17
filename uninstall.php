c278153afae7ce6025e7b9b23e74c98b493c347e
Matt L <9137721+program-the-brain-not-the-heartbeat@users.noreply.github.com>
2024-04-17 10:18:34 -0300

    added uninstall code

--- uninstall.php
+++ uninstall.php
@@ -4,3 +4,15 @@
 if (!defined('WP_UNINSTALL_PLUGIN')) {
 	exit;
 }
+
+/**
+ * Cleanup any left over data from our plugin.
+  * These field keys are defined elsewhere.
+ *
+ * Scheduled hook is in /src/Admin/ImportFeed.php
+ * Transient is stored in/src/Admin/ ImportFeed.php
+ * Option key is in novastream-wp-causeway.php
+ */
+wp_clear_scheduled_hook('cron_import_causeway');
+delete_transient('causeway_data');
+delete_option('novastream-causeway-options');
