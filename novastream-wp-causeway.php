68392e27d7a7d74eadd71cada5a4a0102d9dae37
Matthew Lewis <9137721+program-the-brain-not-the-heartbeat@users.noreply.github.com>
2024-07-11 13:01:25 +0000

    Bumped version to 1.0.9

--- novastream-wp-causeway.php
+++ novastream-wp-causeway.php
@@ -35,7 +35,7 @@ if (!defined('ABSPATH')) {
 
 class CausewayImporter
 {
-    private $version = '1.0.8';
+    private $version = '1.0.9';
     private $slug = 'novastream-wp-causeway';
     private $longName = 'Causeway 5.0 WordPress Importer';
     private $shortName = 'Causeway';
